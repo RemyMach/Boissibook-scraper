06c649e3b231905a2bb22df9577d01875d37e75c
Rémy Machavoine <remygt@hotmail.fr>
2022-05-08 22:57:04 +0200

    feat(scraper): add scraping error to much download 9

--- src/app.py
+++ src/app.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS, cross_origin
 from scraper_book import ScraperImage
 from picture import Picture
+from dotenv import load_dotenv
+from error.to_much_download_error import ToMuchDownloadError
 
-
+load_dotenv('../.env')
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r"": {"origins": "http://localhost:port"}})
@@ -16,10 +18,12 @@ def createPicture():
     picture = request.get_json()
     # convertir le dictionnaire en objet Message
     picture = Picture(picture['name'])
+    try: 
+        file = ScraperImage.imageScrape(picture.name, picture.name)
+    except ToMuchDownloadError:
+        return "can't download"
 
-    ScraperImage.imageScrape(picture.name, picture.name)
-
-    return picture.__dict__
+    return file
 
 
 if __name__ == "__main__":
--- src/scraper_book.py
+++ src/scraper_book.py
@@ -1,8 +1,12 @@
 from selenium import webdriver  # For dynamic web scraping
 from selenium.webdriver.chrome.options import Options   # To add headless option (scrape without a chrome window)
-import os, time
+import os, time, re
 from urllib.request import urlretrieve # To save the scraped images
 from picture import Picture
+from os import listdir
+from os.path import isfile, join
+from error.to_much_download_error import ToMuchDownloadError
+
 
 class ScraperImage:
     
@@ -43,24 +47,37 @@ class ScraperImage:
         driver.get(urlBook)
 
         book_download = driver.find_element_by_xpath(f"//a[@data-isbn='{isbn}']")
+        name_book = driver.find_element_by_xpath(f"//h1[@itemprop='name']").get_attribute("innerHTML").strip()
         url_download = book_download.get_attribute('href')
+        print(name_book)
         print(url_download)
-        driver.get(url_download)
-
-        #If image is fetched correctly, download it and save it in a file
         try:
-            #urlretrieve(url_download, f"static/{isbn}")
-            #encoded = urllib.parse.urlencode(post_data).encode('utf-8')
-            req = urllib.request.Request(url_download)
-            redirected_url = urllib.request.urlopen(req).geturl()
-            print(redirected_url)
-            #urlretrieve('https://swab.zlibcdn.com/dtoken/a7328189a1c7374fd64d1d1044fe1832', f"static/{isbn}.epub")
+            driver.get(url_download)
+            # download-limits-error__header
+            content_page_to_much_download = driver.find_element_by_xpath(f"//h1[@class='download-limits-error__header']").get_attribute("innerHTML").strip()
+            if content_page_to_much_download == "Vous avez atteint votre limite quotidienne":
+                driver.quit()
+                raise ToMuchDownloadError
         except:
-            # An error in the link happened
-            print('[WARNING] Image number %d failed when downloading images for search term: %s' % (count, term))
+            driver.quit()
+            raise ToMuchDownloadError
+    
 
-        for(i in range(0, 60)):
-            
-            time(2)
+        download_directroy = os.environ['DOWNLOAD_PATH']
+
+        while(True):
+            files = [f for f in listdir(download_directroy) if isfile(join(download_directroy, f))]
+            print('-------------------------')
+            count_max_seconds = 60
+            for file in files:
+                #print(file)
+                #print(name_book.replace('\'', ''))
+                if(re.match(r" *"  + name_book.replace('\'', '') + r" *" , file)):
+                    return url_download + '/' + file
+                    
+            time.sleep(2)
+            count_max_seconds -= 2
+            if count_max_seconds == 0:
+                break
         
-        return True
+        return False
